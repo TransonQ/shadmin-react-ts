941c183b16e44e331338c4c5515b53f2a5dc1c22
TransonQ <quanscheng@qq.com>
2024-07-31 12:30:51 +0800

    useDynamicRoutes 优化写法

--- src/hooks/useDynamicRoutes.tsx
+++ src/hooks/useDynamicRoutes.tsx
@@ -19,16 +19,18 @@ export const registerRoutes = {
 export function useDynamicRoutes() {
   const dynamicRoutes: RouteObject[] = []
 
-  const { data, error, isLoading, isValidating } = useSWR(
-    { key: "ACCESS" },
-    ({ key }) => getAccess()
-  )
+  const r = useSWR({ key: "ACCESS" }, ({ key }) => getAccess())
 
-  const tableAccess = data?.access?.["app/example/table"]
-  const formAccess = data?.access?.["app/example/form"]
+  const tableAccess = r.data?.access?.["app/example/table"]
+  const formAccess = r.data?.access?.["app/example/form"]
 
   tableAccess && dynamicRoutes.push(registerRoutes.table)
   formAccess && dynamicRoutes.push(registerRoutes.form)
 
-  return { dynamicRoutes, isLoading, isValidating, error }
+  return {
+    dynamicRoutes,
+    isLoading: r.isLoading,
+    isValidating: r.isValidating,
+    error: r.error,
+  }
 }
