82f4a7297fc41f0bdea76ecb85ff36b6ed5f3e9e
TransonQ <quanscheng@qq.com>
2024-08-06 17:49:28 +0800

    styles: header padding

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -2,7 +2,7 @@ import { Avatar, AvatarFallback } from "@/components"
 
 export const Header = () => {
   return (
-    <div className="w-full h-full flex items-center justify-end">
+    <div className="pr-4 w-full h-full flex items-center justify-end">
       <Avatar>
         <AvatarFallback className="bg-indigo-600 text-background">
           User
