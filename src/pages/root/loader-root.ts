122946adef007586b89170446cbf85659d011acb
TransonQ <quanscheng@qq.com>
2024-08-06 18:11:39 +0800

    chore: login popper

--- src/pages/root/loader-root.ts
+++ src/pages/root/loader-root.ts
@@ -2,9 +2,9 @@ import { popper } from "@/components"
 import { LoaderFunctionArgs, redirect } from "react-router-dom"
 
 export const loaderRoot = async (props: LoaderFunctionArgs) => {
-  console.log("root loader")
-  await new Promise((resolve) => setTimeout(resolve, 2000))
-  popper.success("Authenticated Welcome")
+  // mock checking login
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+  popper.success("Login successfully, welcome!")
 
   return redirect("/app")
 }
