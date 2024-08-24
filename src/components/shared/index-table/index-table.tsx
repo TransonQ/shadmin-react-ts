22d67226f594ebbc2cf38918b3bea5537b4f6cad
TransonQ <quanscheng@qq.com>
2024-08-24 21:22:49 +0800

    feat: index-table

--- src/components/shared/index-table/index-table.tsx
+++ src/components/shared/index-table/index-table.tsx
@@ -1,4 +1,72 @@
-// ? IndexTable
-export const IndexTable = () => {
-  return <div>IndexTable</div>
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui"
+import { cn } from "@/lib"
+import {
+  flexRender,
+  type ColumnDef,
+  type Table as ReactTable,
+} from "@tanstack/react-table"
+interface IndexTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[]
+  table: ReactTable<TData>
+  className?: string
+}
+
+export function IndexTable<TData, TValue>({
+  columns,
+  table,
+  className,
+}: IndexTableProps<TData, TValue>) {
+  return (
+    <div className={cn("rounded-md border", className)}>
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id} colSpan={header.colSpan}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                )
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  )
 }
--- src/pages/example/table/index.tsx
+++ src/pages/example/table/index.tsx
@@ -1,16 +1,71 @@
-import { LegendCard, Page, RowAction } from "@/components/shared"
+import { IndexTable, LegendCard, Page } from "@/components/shared"
+import { generateArray } from "@/lib"
+import { faker } from "@faker-js/faker"
+import type {
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+} from "@tanstack/react-table"
+import {
+  getCoreRowModel,
+  getFacetedRowModel,
+  getFacetedUniqueValues,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table"
+import { useState } from "react"
+import { columns } from "./columns"
+import { FiltersBar } from "./filters-bar"
 
-export const TableExample = () => {
+const fakeData = generateArray(1000, (i) => ({
+  id: `TASK_${i + 1}`,
+  title: faker.lorem.words({ min: 2, max: 6 }),
+  status: faker.helpers.arrayElement([
+    "backlog",
+    "todo",
+    "in progress",
+    "done",
+    "canceled",
+  ]),
+  label: faker.helpers.arrayElement(["Bug", "Feature", "Documentation"]),
+  priority: faker.helpers.arrayElement(["low", "medium", "high"]),
+}))
+
+export function TableExample() {
+  const [rowSelection, setRowSelection] = useState({})
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({})
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
+  const [sorting, setSorting] = useState<SortingState>([])
+
+  const table = useReactTable({
+    data: fakeData,
+    columns,
+    state: {
+      sorting,
+      columnVisibility,
+      rowSelection,
+      columnFilters,
+    },
+    enableRowSelection: true,
+    onRowSelectionChange: setRowSelection,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    onColumnVisibilityChange: setColumnVisibility,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFacetedRowModel: getFacetedRowModel(),
+    getFacetedUniqueValues: getFacetedUniqueValues(),
+  })
+
   return (
     <Page title="TableExample">
       <LegendCard>
-        <RowAction
-          actions={[{ content: "Edit0" }]}
-          sections={[
-            [{ content: "Edit1" }],
-            [{ content: "Edit2", disabled: true }, { content: "Edit3" }],
-          ]}
-        />
+        <FiltersBar />
+        <IndexTable table={table} columns={columns} className="rounded-none" />
       </LegendCard>
     </Page>
   )
