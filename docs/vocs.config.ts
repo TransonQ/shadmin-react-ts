524110a2706ea29dc07a12fda18a78a5671960da
TransonQ <quanscheng@qq.com>
2024-09-05 14:58:28 +0800

    docs: 基本路由

--- docs/vocs.config.ts
+++ docs/vocs.config.ts
@@ -1,15 +1,29 @@
-import { defineConfig } from 'vocs'
+import { defineConfig } from "vocs"
 
 export default defineConfig({
-  title: 'shadmin',
+  title: "shadmin",
   sidebar: [
     {
-      text: 'Getting Started',
-      link: '/getting-started',
+      text: "Getting Started",
+      link: "/getting-started",
     },
     {
-      text: 'Example',
-      link: '/example',
+      text: "Libs",
+      collapsed: true,
+      items: [],
+    },
+    {
+      text: "Base",
+      collapsed: false,
+      items: [
+        { text: "Show", link: "/base/show" },
+        { text: "BlockStack", link: "/base/block-stack" },
+      ],
+    },
+    {
+      text: "Hooks",
+      collapsed: true,
+      items: [],
     },
   ],
 })
